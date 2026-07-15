aab0e97ceeaf893ecee359367c1328d13a081071
moghoneem1122/ <moghoneem556@gmail.com>
2026-07-15 14:45:19 +0300

    Project Structure

---
+++ iOS_GrandHotel/iOS_GrandHotel/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  iOS_GrandHotel
+//
+//  Created by Mohammed on 15/07/2026.
+//
+
+import Foundation
---
+++ iOS_GrandHotel/iOS_GrandHotel/Utilities/Validators.swift
@@ -0,0 +1,8 @@
+//
+//  Validators.swift
+//  iOS_GrandHotel
+//
+//  Created by Mohammed on 15/07/2026.
+//
+
+import Foundation
