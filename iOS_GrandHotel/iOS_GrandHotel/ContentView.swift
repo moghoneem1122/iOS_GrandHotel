a8a88b6a10c103c3f95bc45757cffe4d3471d091
moghoneem1122/ <moghoneem556@gmail.com>
2026-07-15 13:38:44 +0300

    first commit

---
+++ iOS_GrandHotel/iOS_GrandHotel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_GrandHotel
+//
+//  Created by Mohammed on 15/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_GrandHotel/iOS_GrandHotel/iOS_GrandHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GrandHotelApp.swift
+//  iOS_GrandHotel
+//
+//  Created by Mohammed on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GrandHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
