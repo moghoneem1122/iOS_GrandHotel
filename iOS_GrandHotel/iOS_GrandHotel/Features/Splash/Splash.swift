1dfbe0443bd60fa72a9429e8997e2193ed3f2be3
moghoneem1122/ <moghoneem556@gmail.com>
2026-07-26 14:55:42 +0300

    Creation of Font,Image and Localization constants

--- iOS_GrandHotel/iOS_GrandHotel/Features/Splash/Splash.swift
+++ iOS_GrandHotel/iOS_GrandHotel/Features/Splash/Splash.swift
@@ -14,20 +14,16 @@ struct SplashReducer {
     
     @ObservableState
     struct State: Equatable {
-        var isSplashShown: Bool = true
+       
     }
     
     enum Action {
-        case hideSplash
+        
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
-            switch action {
-            case .hideSplash:
-                state.isSplashShown = false
-                return .none
-            }
+            return .none
         }
     }
 }
@@ -42,20 +38,18 @@ struct Splash: View {
             Colors.splashBackGround
                 .ignoresSafeArea(edges: .all)
             VStack(){
-                Image("SplashIcon")
-                    .resizable()
-                    .frame(width: 89.7, height: 123.2)
+                AppImage.logo
+                    .setFrame(width: 89.7, height:123.2)
                 
-                Text("grand_hotel")
-                    .font(.custom("Jost-Bold", size: 40))
+                Text(AppLocalization.splashTitle)
+                    .font(AppFont.splashTitle)
                     .foregroundStyle(.white)
-                    .frame(width: 234, height: 48)
                 
-                Text("find_your_perfect_stay")
-                    .font(.custom("Jost-Regular", size: 14))
+                Text(AppLocalization.splashSubTitle)
+                    .font(AppFont.splashSubTitle)
                     .foregroundStyle(.white)
-                    .fixedSize(horizontal: true, vertical: false)
-            }
+                
+                                }
         }
     }
 }
---
+++ iOS_GrandHotel/iOS_GrandHotel/Utilities/Constants/AppImage.swift
@@ -0,0 +1,13 @@
+//
+//  AppImage.swift
+//  iOS_GrandHotel
+//
+//  Created by Mohammed on 26/07/2026.
+//
+
+import SwiftUI
+
+enum AppImage{
+    //MARK: - SplashView
+    static let logo = Image("SplashIcon")
+}
